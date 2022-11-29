86b6ee4dc81efa734d77aefc1b7f5f23a333adac
Doloquee M. Bedell jr <bedel@RGT-DLT05.roviagatetechnology.local>
2022-11-29 13:41:50 +0000

    Application Completed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,49 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import { Home, Login, Register, NewTicket, Ticket, Tickets } from "./pages";
 
 function App() {
   return (
     <>
-     <h1>Hello from Deco</h1>
+      <Router>
+        <div className="container">
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route
+              path="/new-ticket"
+              element={
+                <PrivateRoute>
+                  <NewTicket />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/tickets"
+              element={
+                <PrivateRoute>
+                  <Tickets />
+                </PrivateRoute>
+              }
+            />
+            <Route
+              path="/ticket/:ticketId"
+              element={
+                <PrivateRoute>
+                  <Ticket />
+                </PrivateRoute>
+              }
+            />
+          </Routes>
+        </div>
+      </Router>
+      <ToastContainer />
     </>
   );
 }
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,15 @@
+import Home from "./Home"
+import Login from "./Login";
+import Register from "./Register";
+import NewTicket from "./NewTicket";
+import Tickets from "./Tickets";
+import Ticket from "./Ticket";
+
+export {
+    Home,
+    Login,
+    Register,
+    NewTicket,
+    Ticket,
+    Tickets
+}
