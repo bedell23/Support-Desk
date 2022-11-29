a241db244bd3c145ec3ea706dd4923c35ae71af4
Doloquee M. Bedell jr <bedel@RGT-DLT05.roviagatetechnology.local>
2022-11-29 08:08:29 +0000

    backend data setup

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
-// app.use("/api/users", require("./routes/userRoutes"));
-// app.use("/api/tickets", require("./routes/ticketRoutes"));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/tickets", require("./routes/ticketRoutes"));
+app.use("/api/notes", require("./routes/noteRoutes"));
 
 // Serve Frontend
 // if (process.env.NODE_ENV === "production") {
